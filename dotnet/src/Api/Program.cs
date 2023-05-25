51db25ba095ee3b6283b560a9280d445a95ed524
João Antunes <joao@antunes.dev>
2023-05-25 21:05:52 +0100

    Update to .NET 8 and add C# Native AOT

--- dotnet/src/Api/Program.cs
+++ dotnet/src/Api/Program.cs
@@ -1,5 +1,10 @@
-using Dapper;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+using Nanorm.Npgsql;
 using Npgsql;
+
+Console.WriteLine(RuntimeFeature.IsDynamicCodeSupported ? "Running with JIT" : "Running with AOT");
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services
@@ -9,16 +14,23 @@ builder.Services
         var connectionString = config.GetConnectionString("SqlConnectionString");
         return () => new NpgsqlConnection(connectionString);
     });
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Clear();
+    // Wire up the JSON source generator and make sure we remove the reflection fallback.
+    // this is a good way to verify that the source generator is working as expected.
+    options.SerializerOptions.TypeInfoResolverChain.Add(SomeThingJsonContext.Default);
+});
 
 var app = builder.Build();
 
+
 app.MapGet("/", async (HttpContext context, IConfiguration configuration, SqlConnectionFactory sqlConnectionFactory) =>
 {
-    // the other stacks are using lower level things than Dapper, but let's keep it realistic,
-    // nobody uses ADO 😛
+    // was using Dapper, but it isn't compatible with AOT, so using Damian Edwards' Nanorm
     await using var connection = sqlConnectionFactory();
-    var item = await connection.QuerySingleAsync<SomeThing>(
-        "SELECT SomeId, SomeText FROM SomeThing LIMIT 1");
+    await connection.OpenAsync();
+    var item = await connection.QuerySingleAsync<SomeThing>("SELECT SomeId, SomeText FROM SomeThing LIMIT 1");
     context.Response.Headers.Add("stack", configuration.GetValue<string>("Stack") ?? "csharp");
     return Results.Ok(item);
 });
@@ -27,4 +39,18 @@ app.Run();
 
 public delegate NpgsqlConnection SqlConnectionFactory();
 
-public record SomeThing(int SomeId, string SomeText);
+public record SomeThing(int SomeId, string SomeText) : IDataReaderMapper<SomeThing>
+{
+    public static SomeThing Map(NpgsqlDataReader dataReader)
+    {
+        return new SomeThing(
+            dataReader.GetInt32(dataReader.GetOrdinal(nameof(SomeId))),
+            dataReader.GetString(dataReader.GetOrdinal(nameof(SomeText))));
+    }
+}
+
+[JsonSerializable(typeof(SomeThing))]
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+public partial class SomeThingJsonContext : JsonSerializerContext
+{
+}
