fa9d92832d4de34d19a6eb70c4a10a98d05858aa
João Antunes <joao@antunes.dev>
2023-11-20 11:53:45 +0000

    Simplify code with Dapper.AOT new version fixes

--- dotnet/src/Api/Program.cs
+++ dotnet/src/Api/Program.cs
@@ -27,29 +27,15 @@ builder.Services.AddSingleton(new Stack(builder.Configuration.GetValue<string>("
 
 var app = builder.Build();
 
-// Dapper.AOT, at least at the moment (it was released like 4 days ago), doesn't seem to be able to handle things if we don't put it in a class
-// app.MapGet("/", async (HttpContext context, SqlConnectionFactory sqlConnectionFactory, Stack stack) =>
-// {
-//     await using var connection = sqlConnectionFactory();
-//     var item = await connection.QuerySingleAsync<SomeThing>("SELECT SomeId, SomeText FROM SomeThing LIMIT 1");
-//     context.Response.Headers["stack"] = stack.Name;
-//     return Results.Ok(item);
-// });
-
-app.MapGet("/", Handlers.Root);
+app.MapGet("/", async (HttpContext context, SqlConnectionFactory sqlConnectionFactory, Stack stack) =>
+{
+    await using var connection = sqlConnectionFactory();
+    var item = await connection.QuerySingleAsync<SomeThing>("SELECT SomeId, SomeText FROM SomeThing LIMIT 1");
+    context.Response.Headers["stack"] = stack.Name;
+    return Results.Ok(item);
+});
 
 app.Run();
-
-public static class Handlers
-{
-    public static async Task<IResult> Root(HttpContext context, SqlConnectionFactory sqlConnectionFactory, Stack stack)
-    {
-        await using var connection = sqlConnectionFactory();
-        var item = await connection.QuerySingleAsync<SomeThing>("SELECT SomeId, SomeText FROM SomeThing LIMIT 1");
-        context.Response.Headers["stack"] = stack.Name;
-        return Results.Ok(item);
-    }    
-}
 
 public delegate NpgsqlConnection SqlConnectionFactory();
 
